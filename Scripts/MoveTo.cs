694e417ee004f666bf506ce947a48f73c8da02dc
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-12-04 09:23:40 +0100

    Scripts de roll a ball

---
+++ Scripts/MoveTo.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform jugador;
+    UnityEngine.AI.NavMeshAgent enemigo;
+    private bool dentro = false;
+    public float proximidad;
+    // el animator
+    Animator animator;
+  
+
+	// Use this for initialization
+	void Start () {
+        enemigo = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        // asiganmos el animator del player para poder cambiar la bool
+        animator = jugador.GetComponent<Animator>();
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dentro = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dentro = false;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+        if (!dentro)
+        {
+            enemigo.destination = jugador.position;
+        }
+        if (dentro)
+        {
+            enemigo.destination = this.transform.position;
+        }
+        // comparamos la distancia al enemigo
+        // cambiamos la variable bool
+        // de esta manera forzamos la transicion desde Tarnquilo a Peligro
+        // y vice versa
+        
+        if (enemigo.remainingDistance < proximidad)
+        {
+            Debug.Log("Peligro");
+            // cambiamos a true la variable del animator
+            animator.SetBool("EstarEnPeligro", true);
+        }
+        else
+        {
+            Debug.Log("Tranqui");
+            // cambiamos a false la variable del animator
+            animator.SetBool("EstarEnPeligro", false);
+        }
+	}
+}
---
+++ Scripts/NewBehaviourScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    public float elapsedTime = 5f;
+    private Rigidbody rb;
+    private int count;
+
+   
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ( "Recoger"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+         if (other.gameObject.tag == "Enemy")
+        {
+            countText.text = "Oughh!!!!!";
+            // vuelve al centro del tablero
+           // player.transform.position = Vector3.zero;
+           //Hacer que el nivel se resetee al detectar la colision con el enemigo
+           elapsedTime += Time.deltaTime;
+            Application.LoadLevel (Application.loadedLevelName);
+            
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 10)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
